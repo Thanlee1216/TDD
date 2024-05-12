633ad44282d65153c1f51b5a7f456becadfdb3c5
LeeTaeHan <lth9312@gmail.com>
2024-05-12 19:28:23 +0900

    구분자로 문자 나눠 더하기

---
+++ src/test/java/com/test/tdd/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.test.tdd.calculator;
+
+public class Calculator {
+
+    public static int calculate(String input) {
+        int result = 0;
+        for(String s : input.split("[\\,,;]")) {
+            result += Integer.parseInt(s);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/test/tdd/calculator/CalculatorTest.java
@@ -0,0 +1,27 @@
+package com.test.tdd.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void 구분자로_문자_나눠_더하기() {
+        String input = "1;2;3";
+        int result = Calculator.calculate(input);
+        assertEquals(6, result);
+
+        input = "1,2;3";
+        result = Calculator.calculate(input);
+        assertEquals(6, result);
+
+        input = "2;2;3";
+        result = Calculator.calculate(input);
+        assertEquals(7, result);
+
+        input = "4,2;3";
+        result = Calculator.calculate(input);
+        assertEquals(9, result);
+    }
+}

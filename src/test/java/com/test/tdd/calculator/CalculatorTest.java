79bc0c5b3105bc04e7c1cddaa578288da57bf5f1
LeeTaeHan <lth9312@gmail.com>
2024-05-12 20:19:45 +0900

    불필요한 변수 선언 제거

--- src/test/java/com/test/tdd/calculator/CalculatorTest.java
+++ src/test/java/com/test/tdd/calculator/CalculatorTest.java
@@ -8,62 +8,48 @@ public class CalculatorTest {
 
     @Test
     void 구분자로_문자_나눠_더하기() {
-        String input = "1:2:3";
-        assertEquals(6, getCalculateResult(input));
+        assertEquals(6, getCalculateResult("1:2:3"));
 
-        input = "1,2:3";
-        assertEquals(6, getCalculateResult(input));
+        assertEquals(6, getCalculateResult("1,2:3"));
 
-        input = "2:2:3";
-        assertEquals(7, getCalculateResult(input));
+        assertEquals(7, getCalculateResult("2:2:3"));
 
-        input = "4,2:3";
-        assertEquals(9, getCalculateResult(input));
+        assertEquals(9, getCalculateResult("4,2:3"));
     }
 
     @Test
     void 커스텀_구분자로_문자_나눠_더하기() {
-        String input = "//;\n1;2;3";
-        assertEquals(6, getCalculateResult(input));
+        assertEquals(6, getCalculateResult("//;\n1;2;3"));
 
-        input = "//,\n1,2,3";
-        assertEquals(6, getCalculateResult(input));
+        assertEquals(6, getCalculateResult("//,\n1,2,3"));
 
-        input = "//[\n3[2[3";
-        assertEquals(8, getCalculateResult(input));
+        assertEquals(8, getCalculateResult("//[\n3[2[3"));
     }
 
     @Test
     void 숫자가_하나만_입력된_경우() {
-        String input = "1";
-        assertEquals(1, getCalculateResult(input));
+        assertEquals(1, getCalculateResult("1"));
 
-        input = "//;\n2";
-        assertEquals(2, getCalculateResult(input));
+        assertEquals(2, getCalculateResult("//;\n2"));
     }
 
     @Test
     void 결과가_int_크기를_벗어난_경우() {
-        String input = "1:2147483647";
-        assertEquals(-1, getCalculateResult(input));
+        assertEquals(-1, getCalculateResult("1:2147483647"));
 
-        input = "//;\n2000000000;2000000000";
-        assertEquals(-1, getCalculateResult(input));
+        assertEquals(-1, getCalculateResult("//;\n2000000000;2000000000"));
     }
 
     @Test
     void 커스텀_구분자와_실제_구분자가_다른_경우() {
-        String input = "//:\n1;2;3";
-        assertEquals(-1, getCalculateResult(input));
+        assertEquals(-1, getCalculateResult("//:\n1;2;3"));
     }
 
     @Test
     void 공백_혹은_NULL이_입력된_경우() {
-        String input = "";
-        assertEquals(-1, getCalculateResult(input));
+        assertEquals(-1, getCalculateResult(""));
 
-        input = null;
-        assertEquals(-1, getCalculateResult(input));
+        assertEquals(-1, getCalculateResult(null));
     }
 
     private int getCalculateResult(String input) {

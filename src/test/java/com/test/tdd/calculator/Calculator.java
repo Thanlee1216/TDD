c1938fb64658c66c807a31decac67f4f0006564f
LeeTaeHan <lth9312@gmail.com>
2024-05-12 20:25:25 +0900

    예외 처리 메소드 이동

--- src/test/java/com/test/tdd/calculator/Calculator.java
+++ src/test/java/com/test/tdd/calculator/Calculator.java
@@ -2,34 +2,42 @@ package com.test.tdd.calculator;
 
 public class Calculator {
 
-    public static int calculate(String input) throws RuntimeException{
+    public static int calculate(String input){
 
-        if(input == "" || input == null) {
-            throw new RuntimeException("잘못된 문자열입니다.");
-        }
-
-        String regexStr = ",,:";
-        if(input.indexOf("\n") != -1) {
-            String[] inputSplit = input.split("\n");
-            regexStr = inputSplit[0].replace("//", "");
-            input = inputSplit[1];
-        }
         int result = 0;
-        int maxResultValue = Integer.MAX_VALUE;
-        for(String s : input.split("[\\" + regexStr + "]")) {
-            int strToInt = 0;
-            try {
-                strToInt = Integer.parseInt(s);
-            }catch (NumberFormatException e) {
+        try {
+            if(input == "" || input == null) {
+                result = -1;
                 throw new RuntimeException("잘못된 문자열입니다.");
             }
-            if(strToInt <= maxResultValue) {
-                maxResultValue -= strToInt;
-                result += Integer.parseInt(s);
-            }else {
-                throw new RuntimeException("int 자료형의 크기를 벗어났습니다.");
+
+            String regexStr = ",,:";
+            if(input.indexOf("\n") != -1) {
+                String[] inputSplit = input.split("\n");
+                regexStr = inputSplit[0].replace("//", "");
+                input = inputSplit[1];
             }
+            int maxResultValue = Integer.MAX_VALUE;
+            for(String s : input.split("[\\" + regexStr + "]")) {
+                int strToInt = 0;
+                try {
+                    strToInt = Integer.parseInt(s);
+                }catch (NumberFormatException e) {
+                    result = -1;
+                    throw new RuntimeException("잘못된 문자열입니다.");
+                }
+                if(strToInt <= maxResultValue) {
+                    maxResultValue -= strToInt;
+                    result += Integer.parseInt(s);
+                }else {
+                    result = -1;
+                    throw new RuntimeException("int 자료형의 크기를 벗어났습니다.");
+                }
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
         }
+
         return result;
     }
 }
--- src/test/java/com/test/tdd/calculator/CalculatorTest.java
+++ src/test/java/com/test/tdd/calculator/CalculatorTest.java
@@ -8,58 +8,40 @@ public class CalculatorTest {
 
     @Test
     void 구분자로_문자_나눠_더하기() {
-        assertEquals(6, getCalculateResult("1:2:3"));
-
-        assertEquals(6, getCalculateResult("1,2:3"));
-
-        assertEquals(7, getCalculateResult("2:2:3"));
-
-        assertEquals(9, getCalculateResult("4,2:3"));
+        assertEquals(6, Calculator.calculate("1:2:3"));
+        assertEquals(6, Calculator.calculate("1,2:3"));
+        assertEquals(7, Calculator.calculate("2:2:3"));
+        assertEquals(9, Calculator.calculate("4,2:3"));
     }
 
     @Test
     void 커스텀_구분자로_문자_나눠_더하기() {
-        assertEquals(6, getCalculateResult("//;\n1;2;3"));
-
-        assertEquals(6, getCalculateResult("//,\n1,2,3"));
-
-        assertEquals(8, getCalculateResult("//[\n3[2[3"));
+        assertEquals(6, Calculator.calculate("//;\n1;2;3"));
+        assertEquals(6, Calculator.calculate("//,\n1,2,3"));
+        assertEquals(8, Calculator.calculate("//[\n3[2[3"));
     }
 
     @Test
     void 숫자가_하나만_입력된_경우() {
-        assertEquals(1, getCalculateResult("1"));
-
-        assertEquals(2, getCalculateResult("//;\n2"));
+        assertEquals(1, Calculator.calculate("1"));
+        assertEquals(2, Calculator.calculate("//;\n2"));
     }
 
     @Test
     void 결과가_int_크기를_벗어난_경우() {
-        assertEquals(-1, getCalculateResult("1:2147483647"));
-
-        assertEquals(-1, getCalculateResult("//;\n2000000000;2000000000"));
+        assertEquals(-1, Calculator.calculate("1:2147483647"));
+        assertEquals(-1, Calculator.calculate("//;\n2000000000;2000000000"));
     }
 
     @Test
     void 커스텀_구분자와_실제_구분자가_다른_경우() {
-        assertEquals(-1, getCalculateResult("//:\n1;2;3"));
+        assertEquals(-1, Calculator.calculate("//:\n1;2;3"));
     }
 
     @Test
     void 공백_혹은_NULL이_입력된_경우() {
-        assertEquals(-1, getCalculateResult(""));
-
-        assertEquals(-1, getCalculateResult(null));
-    }
-
-    private int getCalculateResult(String input) {
-        int result = -1;
-        try {
-            result = Calculator.calculate(input);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
-        return result;
+        assertEquals(-1, Calculator.calculate(""));
+        assertEquals(-1, Calculator.calculate(null));
     }
 
 }

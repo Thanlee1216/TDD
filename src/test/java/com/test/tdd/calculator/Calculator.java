a70565071e9cb1e862ce83d21a54b0f239e5209b
LeeTaeHan <lth9312@gmail.com>
2024-05-12 19:30:42 +0900

    구분자로 문자 나누기 수정 - 기본 규칙 콜론(:)으로 변경

--- src/test/java/com/test/tdd/calculator/Calculator.java
+++ src/test/java/com/test/tdd/calculator/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 
     public static int calculate(String input) {
         int result = 0;
-        for(String s : input.split("[\\,,;]")) {
+        for(String s : input.split("[\\,,:]")) {
             result += Integer.parseInt(s);
         }
         return result;
--- src/test/java/com/test/tdd/calculator/CalculatorTest.java
+++ src/test/java/com/test/tdd/calculator/CalculatorTest.java
@@ -8,20 +8,21 @@ public class CalculatorTest {
 
     @Test
     void 구분자로_문자_나눠_더하기() {
-        String input = "1;2;3";
+        String input = "1:2:3";
         int result = Calculator.calculate(input);
         assertEquals(6, result);
 
-        input = "1,2;3";
+        input = "1,2:3";
         result = Calculator.calculate(input);
         assertEquals(6, result);
 
-        input = "2;2;3";
+        input = "2:2:3";
         result = Calculator.calculate(input);
         assertEquals(7, result);
 
-        input = "4,2;3";
+        input = "4,2:3";
         result = Calculator.calculate(input);
         assertEquals(9, result);
     }
+
 }

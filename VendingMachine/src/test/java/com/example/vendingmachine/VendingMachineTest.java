e43d055a1eb8961697d82a022ecd985c6c892bb1
LeeTaeHan <lth9312@gmail.com>
2024-05-19 19:55:35 +0900

    재고 부족

--- VendingMachine/src/test/java/com/example/vendingmachine/VendingMachineTest.java
+++ VendingMachine/src/test/java/com/example/vendingmachine/VendingMachineTest.java
@@ -13,13 +13,13 @@ public class VendingMachineTest {
         vendingMachine.inputMoney(1000);
         String itemName = vendingMachine.giveItem(1);
         int outputMoney = vendingMachine.outputMoney();
-        assertEquals("코카콜라", itemName);
+        assertEquals("콜라", itemName);
         assertEquals(500, outputMoney);
 
         vendingMachine.inputMoney(1000);
         itemName = vendingMachine.giveItem(2);
         outputMoney = vendingMachine.outputMoney();
-        assertEquals("칠성사이다", itemName);
+        assertEquals("사이다", itemName);
         assertEquals(400, outputMoney);
 
         vendingMachine.inputMoney(1000);
@@ -33,6 +33,23 @@ public class VendingMachineTest {
         outputMoney = vendingMachine.outputMoney();
         assertEquals("물", itemName);
         assertEquals(800, outputMoney);
+    }
+
+    @Test
+    void 재고_부족() {
+        VendingMachine_Beverage vendingMachine = new VendingMachine_Beverage();
+        vendingMachine.inputMoney(1000);
+        String itemName = vendingMachine.giveItem(1);
+        int outputMoney = vendingMachine.outputMoney();
+        assertEquals("콜라", itemName);
+        assertEquals(500, outputMoney);
+
+        vendingMachine.inputMoney(1000);
+        itemName = vendingMachine.giveItem(1);
+        outputMoney = vendingMachine.outputMoney();
+        assertEquals("매진", itemName);
+        assertEquals(1000, outputMoney);
+
     }
 
 }
--- VendingMachine/src/test/java/com/example/vendingmachine/domain/VendingMachine_Beverage.java
+++ VendingMachine/src/test/java/com/example/vendingmachine/domain/VendingMachine_Beverage.java
@@ -3,6 +3,37 @@ package com.example.vendingmachine.domain;
 public class VendingMachine_Beverage{
 
     private int money;
+    private Container_Beverage container_Beverage_Coke;
+    private Container_Beverage container_Beverage_Sprite;
+    private Container_Beverage container_Beverage_Lomonade;
+    private Container_Beverage container_Beverage_Water;
+
+    public VendingMachine_Beverage(){
+        container_Beverage_Coke = new Container_Beverage();
+        container_Beverage_Sprite = new Container_Beverage();
+        container_Beverage_Lomonade = new Container_Beverage();
+        container_Beverage_Water = new Container_Beverage();
+
+        container_Beverage_Coke.setItemId(1);
+        container_Beverage_Sprite.setItemId(2);
+        container_Beverage_Lomonade.setItemId(3);
+        container_Beverage_Water.setItemId(4);
+
+        container_Beverage_Coke.setItemName("콜라");
+        container_Beverage_Sprite.setItemName("사이다");
+        container_Beverage_Lomonade.setItemName("레몬에이드");
+        container_Beverage_Water.setItemName("물");
+
+        container_Beverage_Coke.setItemCount(1);
+        container_Beverage_Sprite.setItemCount(1);
+        container_Beverage_Lomonade.setItemCount(1);
+        container_Beverage_Water.setItemCount(1);
+
+        container_Beverage_Coke.setItemPrice(500);
+        container_Beverage_Sprite.setItemPrice(600);
+        container_Beverage_Lomonade.setItemPrice(700);
+        container_Beverage_Water.setItemPrice(200);
+    }
 
     public void inputMoney(int money) {
         this.money = money;
@@ -14,17 +45,25 @@ public class VendingMachine_Beverage{
 
     public String giveItem(int itemId) {
         if(itemId == 1) {
-            this.money -= 500;
-            return "코카콜라";
+            if(container_Beverage_Coke.getItemCount() <= 0) return "매진";
+            this.money -= container_Beverage_Coke.getItemPrice();
+            container_Beverage_Coke.setItemCount(container_Beverage_Coke.getItemCount() - 1);
+            return container_Beverage_Coke.getItemName();
         } else if(itemId == 2) {
-            this.money -= 600;
-            return "칠성사이다";
+            if(container_Beverage_Sprite.getItemCount() <= 0) return "매진";
+            this.money -= container_Beverage_Sprite.getItemPrice();
+            container_Beverage_Sprite.setItemCount(container_Beverage_Sprite.getItemCount() - 1);
+            return container_Beverage_Sprite.getItemName();
         }else if(itemId == 3) {
-            this.money -= 700;
-            return "레몬에이드";
+            if(container_Beverage_Lomonade.getItemCount() <= 0) return "매진";
+            this.money -= container_Beverage_Lomonade.getItemPrice();
+            container_Beverage_Lomonade.setItemCount(container_Beverage_Lomonade.getItemCount() - 1);
+            return container_Beverage_Lomonade.getItemName();
         }else if(itemId == 4) {
-            this.money -= 200;
-            return "물";
+            if(container_Beverage_Water.getItemCount() <= 0) return "매진";
+            this.money -= container_Beverage_Water.getItemPrice();
+            container_Beverage_Water.setItemCount(container_Beverage_Water.getItemCount() - 1);
+            return container_Beverage_Water.getItemName();
         }else {
             return "없음";
         }
